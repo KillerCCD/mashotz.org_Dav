0163f091a9d5147b7ce78cce026e7096340c5bc6
Davit0012 <dadas.dadas.09@mail.ru>
2022-03-05 15:08:12 +0400

    From Davo

---
+++ lib/ui/widgets/head_page/home_screen.dart
@@ -0,0 +1,249 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:mashtoz_flutter/config/palette.dart';
+
+import '../buttons/bottom_navigation_bar/bottom_app_bar.dart';
+import './menu_pages/menu_pages.dart';
+
+//enum BottomIcons { Home, Favorite, Search, Account }
+enum BottomIcons { home, library, search, italian, account, initall }
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  BottomIcons icons = BottomIcons.initall;
+  int _currentIndex = 0;
+  bool isHome = false;
+  bool isLibrary = false;
+  bool isSearch = false;
+  bool isItalian = false;
+  bool isAccount = false;
+
+  final pages = const [
+    HomePage(),
+    LibraryPage(),
+    SearchPage(),
+    ItalianPage(),
+    AccountPage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: pages[_currentIndex],
+        resizeToAvoidBottomInset: false,
+        bottomNavigationBar: buildMyNavBar(context));
+  }
+
+  SizedBox buildMyNavBar(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      // padding: const EdgeInsets.only(top: 14),
+      child: Stack(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 20,
+            // color: Colors.black,
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icons == BottomIcons.home
+                    ? Expanded(
+                        child: CustomPaint(
+                          size: const Size(42, 22),
+                          painter: Triangle(),
+                        ),
+                      )
+                    : Expanded(child: Container()),
+                icons == BottomIcons.library
+                    ? Expanded(
+                        child: CustomPaint(
+                          size: const Size(42, 22),
+                          painter: Triangle(),
+                        ),
+                      )
+                    : Expanded(child: Container()),
+                icons == BottomIcons.search
+                    ? Expanded(
+                        child: CustomPaint(
+                          size: const Size(42, 22),
+                          painter: Triangle(),
+                        ),
+                      )
+                    : Expanded(child: Container()),
+                icons == BottomIcons.italian
+                    ? Expanded(
+                        child: CustomPaint(
+                          size: const Size(42, 22),
+                          painter: Triangle(),
+                        ),
+                      )
+                    : Expanded(child: Container()),
+                icons == BottomIcons.account
+                    ? Expanded(
+                        child: CustomPaint(
+                          size: const Size(42, 22),
+                          painter: Triangle(),
+                        ),
+                      )
+                    : Expanded(child: Container()),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            height: 100,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  child: SizedBox.expand(
+                    child: Container(
+                      height: 55,
+                      color: const Color.fromRGBO(83, 66, 77, 1),
+                      child: Row(
+                        children: [
+                          // const SizedBox(width: 22.0),
+                          Expanded(
+                            child: BottomBar(
+                              onPressed: () {
+                                print('home');
+                                setState(() {
+                                  _currentIndex = 0;
+                                  icons = BottomIcons.home;
+                                  isHome = !isHome;
+                                  isLibrary = false;
+                                  isSearch = false;
+                                  isItalian = false;
+                                  isAccount = false;
+                                });
+                              },
+                              bottomIcons:
+                                  icons == BottomIcons.home ? true : false,
+                              path: SvgPicture.asset(
+                                'assets/images/home.svg',
+                                color: isHome ? Palette.cursor : null,
+                                width: 25,
+                                height: 30,
+                              ),
+                            ),
+                          ),
+                          //  const SizedBox(width: 45.0),
+                          Expanded(
+                            child: BottomBar(
+                              onPressed: () {
+                                print('library');
+                                setState(() {
+                                  icons = BottomIcons.library;
+                                  _currentIndex = 1;
+                                  isLibrary = !isLibrary;
+                                  isHome = false;
+                                  isSearch = false;
+                                  isItalian = false;
+                                  isAccount = false;
+                                });
+                              },
+                              bottomIcons:
+                                  icons == BottomIcons.library ? true : false,
+                              path: SvgPicture.asset(
+                                'assets/images/library.svg',
+                                color: isLibrary ? Palette.cursor : null,
+                                width: 25,
+                                height: 30,
+                              ),
+                            ),
+                          ),
+                          //const SizedBox(width: 45.0),
+                          Expanded(
+                            child: BottomBar(
+                                onPressed: () {
+                                  print('search');
+                                  setState(() {
+                                    _currentIndex = 2;
+                                    icons = BottomIcons.search;
+                                    isSearch = !isSearch;
+                                    isHome = false;
+                                    isLibrary = false;
+                                    isItalian = false;
+                                    isAccount = false;
+                                  });
+                                },
+                                bottomIcons:
+                                    icons == BottomIcons.search ? true : false,
+                                path: SvgPicture.asset(
+                                  'assets/images/search.svg',
+                                  color: isSearch ? Palette.cursor : null,
+                                  width: 25,
+                                  height: 27,
+                                )),
+                          ),
+                          // const SizedBox(width: 45.0),
+                          Expanded(
+                            child: BottomBar(
+                              onPressed: () {
+                                print('italian');
+                                setState(() {
+                                  icons = BottomIcons.italian;
+                                  _currentIndex = 3;
+                                  isItalian = !isItalian;
+                                  isHome = false;
+                                  isLibrary = false;
+                                  isSearch = false;
+                                  isAccount = false;
+                                });
+                              },
+                              bottomIcons:
+                                  icons == BottomIcons.italian ? true : false,
+                              path: SvgPicture.asset(
+                                'assets/images/italian_lessons.svg',
+                                color: isItalian ? Palette.cursor : null,
+                                width: 25,
+                                height: 30,
+                              ),
+                            ),
+                          ),
+                          // const SizedBox(width: 40.0),
+                          Expanded(
+                            child: BottomBar(
+                              onPressed: () {
+                                print('account');
+                                setState(() {
+                                  _currentIndex = 4;
+                                  icons = BottomIcons.account;
+                                  isAccount = !isAccount;
+                                  isHome = false;
+                                  isLibrary = false;
+                                  isSearch = false;
+                                  isItalian = false;
+                                });
+                              },
+                              bottomIcons:
+                                  icons == BottomIcons.account ? true : false,
+                              path: SvgPicture.asset(
+                                'assets/images/profile.svg',
+                                color: isAccount ? Palette.cursor : null,
+                                width: 25,
+                                height: 27,
+                              ),
+                            ),
+                          ),
+                          //const SizedBox(width: 40.0),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/head_page/menu_pages/menu_pages.dart
@@ -0,0 +1,5 @@
+export './account_page.dart';
+export './home_page.dart';
+export './italian_lesson_page.dart';
+export './library_page.dart';
+export './search_page.dart';
